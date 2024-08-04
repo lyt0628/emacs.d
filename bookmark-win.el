7ec52ea96c8b4f6eb87fe9fa4c5c9ebfa9ec87c8
lyt0628 <lyt.0628@qq.com>
2024-08-05 01:46:28 +0800

    dayly: 24-8-5

---
+++ bookmark-win.el
@@ -0,0 +1,11 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: utf-8-emacs; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("org-capture-last-stored"
+ (filename . "d:/data/emacs.d/org/roam/winapi_makelong_makeword-2024-08-04.org")
+ (front-context-string)
+ (rear-context-string . "ast_modified:  \n")
+ (position . 145)
+ (last-modified 26287 21795 382227 0))
+)
