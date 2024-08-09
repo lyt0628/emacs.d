5e95cb64f7e4593265350bfd86732a7725ba66a2
lyt0628 <lyt.0628@qq.com>
2024-08-09 22:48:15 +0800

    dayly: 24-8-9

--- bookmark-win.el
+++ bookmark-win.el
@@ -3,9 +3,9 @@
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
 (("org-capture-last-stored"
- (filename . "d:/data/emacs.d/org/roam/cs_ds_stack-2024-08-07.org")
+ (filename . "d:/data/emacs.d/org/roam/js_async-2024-08-08.org")
  (front-context-string)
  (rear-context-string . "ast_modified:  \n")
- (position . 132)
- (last-modified 26290 48679 206360 0))
+ (position . 130)
+ (last-modified 26292 22561 278184 0))
 )
