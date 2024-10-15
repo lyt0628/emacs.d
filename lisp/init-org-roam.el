0c379df8be5362a1fb013975e27bf87bfffcefee
lyt0628 <LYT.0628@qq.com>
2024-10-15 15:26:00 +0800

    load org-roam-ui in graph oprn

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -90,16 +90,17 @@ it can be passed in POS."
    )
 
 (use-package org-roam-ui
-    :after org-roam
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-    ;;  :hook (after-init . org-roam-ui-mode)
-    :custom
-    (org-roam-ui-sync-theme t)
-    (org-roam-ui-follow t)
-    (org-roam-ui-update-on-save t)
-    (org-roam-ui-open-on-start t))
+  :if display-graphic-p
+  :after org-roam
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :custom
+  (org-roam-ui-sync-theme t)
+  (org-roam-ui-follow t)
+  (org-roam-ui-update-on-save t)
+  (org-roam-ui-open-on-start t))
 
 (provide 'init-org-roam)
 
